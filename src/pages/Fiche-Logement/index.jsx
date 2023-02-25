e731361f974db76dcc02d2f546d1489e71becc29
Gaetan <gaetan.gautier61@free.fr>
2023-02-25 03:18:24 +0100

    Page Fiche-Logement : Add first line with title + Avatar

--- src/pages/Fiche-Logement/index.jsx
+++ src/pages/Fiche-Logement/index.jsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router"
 import Carousel from "../../components/Carousel"
 import Header from "../../components/Header"
 import { LogementsList } from "../../datas/logements"
+import "../Fiche-Logement/fiche-logement.css"
 
 function FicheLogement() {
   const { id } = useParams()
@@ -16,6 +17,26 @@ function FicheLogement() {
       <Header />
       <div>
         <Carousel images={logement.pictures} />
+      </div>
+      <div className="flex justify-space-between mt-30">
+        <div className="flex flex-column">
+          <h2 className="font-color m-0">{logement.title}</h2>
+          <p className="font-color m-0 weight500">{logement.location}</p>
+        </div>
+        <div className="flex">
+          <div className="flex flex-column justify-center mr-10">
+            {logement.host.name.split(" ").map((item) => (
+              <p className="flex justify-flex-end font-color m-0 weight500">
+                {item}
+              </p>
+            ))}
+          </div>
+          <img
+            className="image-avatar"
+            src={logement.host.picture}
+            alt={logement.host.name}
+          ></img>
+        </div>
       </div>
     </div>
   )
